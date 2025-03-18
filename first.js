867a548d78e01c258436affd2202f8d673cd6098
martibarraco <martibarraco21@gmail.com>
2025-03-18 12:42:22 -0300

    hola

--- first.js
+++ first.js
@@ -1,1 +1,11 @@
+console.log ("hola");
+async function main() {
+    const req = await fetch ("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
+    if (req.ok) {
+        const data = await req.jason();
+        console.log (data);
+    }
+}
+
+main ()
 
